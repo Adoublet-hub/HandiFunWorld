c69e4e896bfbee996311423a7bfe53b2359e896c
adoublet <doubletamandine1@gmail.com>
2025-02-08 15:53:56 +0100

    init server

--- main.go
+++ main.go
@@ -1,3 +1,25 @@
 package main
 
-import ""
+import (
+	"fmt"
+	"net/http"
+)
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	http.ServeFile(w, r, "template/accueil.html")
+}
+
+func main() {
+	http.HandleFunc("/", handler)
+
+	// Permet de servir les fichiers CSS
+	http.Handle("/style/", http.StripPrefix("/style/", http.FileServer(http.Dir("style"))))
+
+	port := ":8080"
+	fmt.Println("Serveur démarré sur http://localhost" + port)
+
+	err := http.ListenAndServe(port, nil)
+	if err != nil {
+		fmt.Println("Erreur:", err)
+	}
+}
