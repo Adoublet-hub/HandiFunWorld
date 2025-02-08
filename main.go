9226ab52e3b8de642f89b5ac41ed5b81eef235c2
adoublet <doubletamandine1@gmail.com>
2025-02-08 18:20:25 +0100

    fix: Update page handlers

--- main.go
+++ main.go
@@ -5,13 +5,51 @@ import (
 	"net/http"
 )
 
-func handler(w http.ResponseWriter, r *http.Request) {
+// Handler pour la page d'accueil
+func accueilHandler(w http.ResponseWriter, r *http.Request) {
 	http.ServeFile(w, r, "template/accueil.html")
 }
 
-func main() {
-	http.HandleFunc("/", handler)
+func equipeHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Handler pour /equipe appelé")
+	http.ServeFile(w, r, "template/equipe.html")
+}
 
+func evenementsHandler(w http.ResponseWriter, r *http.Request) {
+	http.ServeFile(w, r, "template/evenements.html")
+}
+
+func funnyHandler(w http.ResponseWriter, r *http.Request) {
+	http.ServeFile(w, r, "template/funny.html")
+}
+
+func handinfoHandler(w http.ResponseWriter, r *http.Request) {
+	http.ServeFile(w, r, "template/handinfo.html")
+}
+
+func hnpwHandler(w http.ResponseWriter, r *http.Request) {
+	http.ServeFile(w, r, "template/hnpw.html")
+}
+
+func partenairesHandler(w http.ResponseWriter, r *http.Request) {
+	http.ServeFile(w, r, "template/partenaires.html")
+}
+
+func storytimeHandler(w http.ResponseWriter, r *http.Request) {
+	http.ServeFile(w, r, "template/storytime.html")
+}
+
+
+func main() {
+
+	http.HandleFunc("/", accueilHandler)    // Page d'accueil
+	http.HandleFunc("/equipe", equipeHandler) // Page L'équipe
+	http.HandleFunc("/evenements", evenementsHandler)
+	http.HandleFunc("/funny", funnyHandler)
+	http.HandleFunc("/handinfo", handinfoHandler)
+	http.HandleFunc("/hnpw", hnpwHandler)
+	http.HandleFunc("/partenaires", partenairesHandler)
+	http.HandleFunc("/storytime", storytimeHandler)
 	// Permet de servir les fichiers CSS
 	http.Handle("/style/", http.StripPrefix("/style/", http.FileServer(http.Dir("style"))))
 
