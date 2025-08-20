974bbe21de24cf80cb68d05fcd42dd3202a30a11
adoublet <doubletamandine1@gmail.com>
2025-08-20 16:09:36 +0200

    front card partenaire

--- js/script.js
+++ js/script.js
@@ -17,4 +17,28 @@ document.addEventListener('DOMContentLoaded', () => {
   });
 });
 
+// Sélection du popup
+const popup = document.getElementById("imgPopup");
+const popupImg = document.getElementById("popupImg");
+const closePopup = popup.querySelector(".close");
+
+// Cibler toutes les images de la face arrière
+document.querySelectorAll(".card-back img").forEach(img => {
+  img.addEventListener("click", () => {
+    popup.style.display = "flex"; // affiche le popup
+    popupImg.src = img.src;       // met la bonne image
+  });
+});
+
+// Fermer le popup avec la croix
+closePopup.addEventListener("click", () => {
+  popup.style.display = "none";
+});
+
+// Fermer si on clique en dehors de l'image
+popup.addEventListener("click", e => {
+  if (e.target === popup) {
+    popup.style.display = "none";
+  }
+});
 
