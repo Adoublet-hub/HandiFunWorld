4576f49fa3854a1f2d78f75ea277c300db3b5efd
adoublet <doubletamandine1@gmail.com>
2025-09-09 15:21:12 +0200

    page storytime

--- js/story.js
+++ js/story.js
@@ -1,13 +1,15 @@
-document.querySelectorAll(".story-header").forEach(button => {
+document.querySelectorAll(".story-toggle").forEach(button => {
   button.addEventListener("click", () => {
     const content = button.nextElementSibling;
 
     if (content.style.maxHeight) {
       content.style.maxHeight = null;
       content.style.padding = "0 15px";
+      button.textContent = "Clique pour en savoir plus";
     } else {
       content.style.maxHeight = content.scrollHeight + "px";
       content.style.padding = "15px";
+      button.textContent = "Réduire";
     }
   });
 });
